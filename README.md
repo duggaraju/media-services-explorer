602412f949ccd6aa9bcbc21f4d4575b7148b000f
Prakash Duggaraju <duggaraju@gmail.com>
2017-02-07 01:05:08 -0800

    Fix build errors and update README.md

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ This project was generated with [angular-cli](https://github.com/angular/angular
 ## Development server
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
+## Run electron
+Run `npm run electron` to start the electron application.  Start the development server before.
+
 ## Code scaffolding
 
 Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive/pipe/service/class/module`.
--- src/app/account/keys/keys.component.ts
+++ src/app/account/keys/keys.component.ts
@@ -2,11 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EntityComponent } from '../entity.component';
 import { ContentKey } from '../../media/contentkey';
-import { QueryResult } from '../../media/queryresult';
 import { MediaServiceFactory } from '../../media/media.service.factory';
-import { Observable } from 'rxjs';
 import { AccountService } from '../../account.service';
-import { ContextMenuService } from 'angular2-contextmenu';
+import { ContextMenuService, ContextMenuComponent } from 'angular2-contextmenu';
 
 @Component({
   selector: 'app-keys',
@@ -29,10 +27,6 @@ export class KeysComponent extends EntityComponent<ContentKey> {
   ]
   
   constructor(activatedRoute: ActivatedRoute, mediaServiceFactory: MediaServiceFactory, accountService: AccountService, contextMenuService: ContextMenuService) {
-    super(activatedRoute, mediaServiceFactory, accountService, contextMenuService);
-   }
-
-   queryEntities(): Observable<QueryResult<ContentKey>> {
-     return this.mediaService.getContentKeys(this.query);
+    super(activatedRoute, mediaServiceFactory, accountService, contextMenuService, "ContentKeys");
    }
 }
